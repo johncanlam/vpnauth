f77dadda36e13e64c362946b0a4fb7cd3e98f7b0
Johncan Lam <johncanlam@gmail.com>
2012-06-10 10:31:43 +0800

    edit install document
    1.edit install document
    2.add search in Log manage
    3、add some script and conf

--- vpnauth/vpn/admin.py
+++ vpnauth/vpn/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 class VpnLogAdmin(admin.ModelAdmin):
     list_display = ['username','content','login_time','result']
     list_filter = ['login_time','result']
-
+    search_fields = ['username']
 
 admin.site.register(VpnLog, VpnLogAdmin)
